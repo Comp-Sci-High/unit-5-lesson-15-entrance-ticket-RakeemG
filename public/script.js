2bada99169807ecc3874fee093c1fdeb2f6f1122
RakeemG <rakeem.grace25@compscihigh.org>
2025-04-08 14:01:25 +0000

    Finished

--- public/script.js
+++ public/script.js
@@ -1,7 +1,19 @@
 // Create a function that tests the post route handler [2 pts]
 
+async function testPostRoute(url, data) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+    return await response.json();
+}
 
 
 // Call the function [1 pt]
+
+
 // Show us the successful POST fetch [1 pt]
 
